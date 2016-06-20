fc0358da997a886e8621306f45694e5be7c231e9
Dayo Williams <Dayo Williams>
2016-06-20 15:20:15 +0100

    Second commit

--- client/main.js
+++ client/main.js
@@ -1,22 +1,52 @@
 import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import { ReactiveDict } from 'meteor/reactive-dict';
 
 import './main.html';
+import { Tasks } from '../lib/task.js';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+Template.body.onCreated(function bodyOnCreated() {
+	this.state = new ReactiveDict();
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
+Template.body.helpers({
+	tasks: function() {
+		const instance = Template.instance();
+		if (instance.state.get('hideCompleted')){
+			// if hide completed is checked, filter tasks
+			return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt:-1 } });
+		}
+		// otherwise, return all of the tasks
+		return Tasks.find({}, {sort : { text: -1} });
+	},
+	incompleteCount() {
+		return Tasks.find({ checked: { $ne: true } }).count();
+	},
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Template.body.events({
+	'submit .new-task'(event) {
+		event.preventDefault();
+
+		const target = event.target;
+		const text = target.text.value;
+
+		Tasks.insert({
+			text,
+			createdAt: new Date(),
+		});
+
+		target.text.value =''
+	},
+	'click .toggle-checked'() {
+
+		Tasks.update(this._id, {
+			$set: { checked: ! this.checked }
+		});
+	},
+	'click .delete'() {
+		Tasks.remove(this._id);
+	},
+	'change .hide-completed input' (event, instance) {
+		instance.state.set('hideCompleted', event.target.checked);
+	},
 });
